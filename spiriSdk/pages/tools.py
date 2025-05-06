7a5e6c2229cfa56ec19b70f2929ce8e83c0aac37
SidneyCW <Sidney.scott.wood@gmail.com>
2025-05-06 15:45:06 -0400

    eod day 1 changed some styles and tools page now sends commands

--- spiriSdk/pages/tools.py
+++ spiriSdk/pages/tools.py
@@ -1,7 +1,28 @@
-from nicegui import ui
-from pages.header import header
+from nicegui import ui, binding, app, run
+from spiriSdk.pages.header import header
+import time
+import docker
+import subprocess
+import asyncio
+
+applications = {
+    'rqt': ['rqt'],
+    'rvis2': ['rviz2'],
+    'Gazebo': ['gazebo']
+}
+
+def launch_app(command):
+    try:
+        subprocess.Popen(command)
+    except FileNotFoundError:
+        print(f"Command not found: {command}. Make sure it is installed and available in the PATH.")
+
 
 @ui.page('/tools')
 async def tools():
     await header()
-    
+    with ui.grid(columns=3):
+        for app_name, command in applications.items():
+            with ui.card().props('flat').classes('justify-center items-center'):
+                ui.label(app_name).tailwind.font_weight('extrabold')
+                ui.button('Launch', on_click=lambda cmd=command: launch_app(cmd), color='#a0dbea').props('ripple icon-right="img:/icons/arrow_circle_right.png"').tailwind.font_weight('extrabold')
