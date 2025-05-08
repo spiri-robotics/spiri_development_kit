c57c439df89e525ebdfa6add1ccd835064dca90b
mayflower <elena@spirirobotics.com>
2025-05-08 17:10:18 -0300

    restructured how cards are constructed

--- spiriSdk/pages/manage_robots.py
+++ spiriSdk/pages/manage_robots.py
@@ -10,34 +10,66 @@ async def manage_robots():
     await styles()
     await header()
 
+    daemons = []
+
+    with ui.dialog() as editRobot, ui.card():
+        await edit_robot()
+
+        with ui.card_actions().props('align=stretch'):
+            ui.button('Save', on_click=editRobot.close)
+            ui.button('Cancel', on_click=editRobot.close)
+
+    def add_card():
+        addRobot.close()
+        with ui.card().classes('w-full'):
+            with ui.row(align_items='stretch').classes('w-full'):
+                with ui.card_section():
+                    ui.label('h').classes('mb-5')
+                    ui.label('[status]').classes('mt-5')
+                ui.space()
+                with ui.card_actions():
+                    ui.button('Start', icon='play_arrow', color='positive').classes('m-1')
+                    ui.button('Stop', icon='stop', color='warning').classes('m-1')
+                    ui.button('Restart', icon='refresh', color='secondary').classes('m-1 mr-10')
+
+                    with ui.dropdown_button(icon='settings', color='secondary'):
+                        ui.item('Edit', on_click=editRobot.open)
+                        ui.item('Delete')
+
     with ui.dialog() as addRobot, ui.card(align_items='stretch').classes('w-full'):
         await new_robots()
 
         with ui.card_actions().props('align=center'):
-            ui.button('Add', color='secondary', on_click=addRobot.close)
             ui.button('Cancel', color='secondary', on_click=addRobot.close)
+            ui.button('Add', color='secondary', on_click=add_card)
+       
 
     ui.button('Add Robot', on_click=addRobot.open, color='secondary')
     ui.button('actual add robot page', on_click=lambda: ui.navigate.to('/new_robots'), color='secondary')
+    ui.card().classes('w-full').tight().classes('w-full')
 
-    with ui.card().classes('w-full'):
-        with ui.row(align_items='stretch').classes('w-full'):
-            with ui.card_section():
-                ui.label('[robot name]').classes('mb-5')
-                ui.label('[status]').classes('mt-5')
-            ui.space()
-            with ui.card_actions():
-                ui.button('Start', icon='play_arrow', color='positive').classes('m-1')
-                ui.button('Stop', icon='stop', color='warning').classes('m-1')
-                ui.button('Restart', icon='refresh', color='secondary').classes('m-1 mr-10')
+    class RobotCard:
 
-                with ui.dialog() as editRobot, ui.card():
-                    await edit_robot()
+        def __init__(self) -> None:
+            self.card = ui.card().tight().classes('w-full')
 
-                    with ui.card_actions().props('align=stretch'):
-                        ui.button('Save', on_click=editRobot.close)
-                        ui.button('Cancel', on_click=editRobot.close)
 
-                with ui.dropdown_button(icon='settings', color='secondary'):
-                    ui.item('Edit', on_click=editRobot.open)
-                    ui.item('Delete')
+        def add_card(self) -> None:
+            with self.card:
+                with ui.card().classes('w-full'):
+                    with ui.row(align_items='stretch').classes('w-full'):
+                        with ui.card_section():
+                            ui.label(f'bob').classes('mb-5')
+                            ui.label(f'active').classes('mt-5')
+                        ui.space()
+                        with ui.card_actions():
+                            ui.button('Start', icon='play_arrow', color='positive').classes('m-1')
+                            ui.button('Stop', icon='stop', color='warning').classes('m-1')
+                            ui.button('Restart', icon='refresh', color='secondary').classes('m-1 mr-10')
+
+                            with ui.dropdown_button(icon='settings', color='secondary'):
+                                ui.item('Edit', on_click=editRobot.open)
+                                ui.item('Delete')
+
+    card = RobotCard()
+    ui.button('Add label to card', on_click=card.add_card)
