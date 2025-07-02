ad7cae510a01cec01e21aa5299e316495182ddc1
mayflower <elena@spirirobotics.com>
2025-07-02 18:25:55 +0000

    moved card functions out of displaycards + code cleanup

--- spiriSdk/utils/card_utils.py
+++ spiriSdk/utils/card_utils.py
@@ -49,6 +49,124 @@ async def addRobot():
             ).classes('text-base').bind_enabled_from(checker, 'isValid')
     
     d.open()
+    
+def update_status(name, label: ui.label):
+    status = display_daemon_status(name)
+    label.text = f'{status.capitalize()}'
+    if status == 'running':
+        label.classes('text-[#609926]')
+    elif status == 'stopped':
+        label.classes('text-[#d43131]')
+    return status
+
+def start_polling(name, label, gz_toggle: ToggleButton):
+    async def polling_loop():
+        while True:
+            status = update_status(name, label)
+            world_running = await get_running_worlds()
+            if gz_toggle:
+                if status == 'running' and len(world_running) > 0:
+                    gz_toggle.visible = True
+                elif gz_toggle.visible == True:
+                    gz_toggle.visible = False
+                    await remove_from_world(name)
+                if not is_robot_alive(name):
+                    gz_toggle.state = False
+                    gz_toggle.update()
+            if len(world_running) == 0:
+                gz_world.models = {}
+            await asyncio.sleep(3)
+    asyncio.create_task(polling_loop())
+    
+async def power_on(robot, buttons: list):
+    for button in buttons:
+        button.disable()
+    n = ui.notification(timeout=None)
+    for i in range(1):
+        n.message = 'Powering on...'
+        n.spinner = True
+        await asyncio.sleep(1)
+        
+    await start_container(robot)
+    
+    n.message = f'Container {robot} started'
+    n.type = 'positive'
+    n.spinner = False
+    n.timeout = 4
+    
+    displayCards.refresh()
+
+async def power_off(robot, buttons: list):
+    for button in buttons:
+        button.disable()
+    n = ui.notification(timeout=None)
+    for i in range(1):
+        n.message = 'Powering off...'
+        n.spinner = True
+        await asyncio.sleep(1)
+        
+    message, type = stop_container(robot)
+    
+    n.message = message
+    n.type = type
+    n.spinner = False
+    n.timeout = 4
+    
+    displayCards.refresh()
+
+async def reboot(robot, buttons: list):
+    for button in buttons:
+        button.disable()
+    n = ui.notification(message='Rebooting...', spinner=True, timeout=None)
+
+    await restart_container(robot)
+    
+    n.message = 'Done'
+    n.spinner = False
+    n.type = 'positive'
+    n.timeout = 4
+    
+    displayCards.refresh()
+    
+async def add_to_world(robot):
+    try:
+        robotType = "_".join(str(robot).split('_')[0:-1])
+        print(robotType)
+        await gz_world.prep_bot(robot, robotType)
+        running_worlds = await get_running_worlds()
+        if len(running_worlds) > 0:
+            ui.notify(f'Added {robot} to world')
+            return True
+        else:
+            raise Exception('No world running')
+    except Exception as e:
+        print(e)
+        return False
+
+async def remove_from_world(robot):
+    try:
+        robot = gz_world.models[robot].kill_model()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+    
+async def delete(robot):
+    n = ui.notification(timeout=False)
+    for i in range(1):
+        n.message = 'Deleting...'
+        n.spinner=True
+        await asyncio.sleep(0.1)
+
+    if await delete_robot(robot):
+        n.message = f'{robot} deleted'
+        n.type = 'positive'
+    else:
+        n.message = 'error deleting robot'
+        n.type = 'negative'
+    
+    n.spinner = False
+    n.timeout = 4
                         
 @ui.refreshable
 def displayCards():
@@ -67,125 +185,6 @@ def displayCards():
                         alias = line.split('=', 1)
                         alias = alias[1].strip()
                         break
-
-            # Functions
-            def update_status(name, label: ui.label):
-                status = display_daemon_status(name)
-                label.text = f'{status.capitalize()}'
-                if status == 'running':
-                    label.classes('text-[#609926]')
-                elif status == 'stopped':
-                    label.classes('text-[#d43131]')
-                return status
-
-            def start_polling(name, label, gz_toggle: ToggleButton):
-                async def polling_loop():
-                    while True:
-                        status = update_status(name, label)
-                        world_running = await get_running_worlds()
-                        if gz_toggle:
-                            if status == 'running' and len(world_running) > 0:
-                                gz_toggle.visible = True
-                            elif gz_toggle.visible == True:
-                                gz_toggle.visible = False
-                                await remove_from_world(name)
-                            if not is_robot_alive(name):
-                                gz_toggle.state = False
-                                gz_toggle.update()
-                        if len(world_running) == 0:
-                            gz_world.models = {}
-                        await asyncio.sleep(3)
-                asyncio.create_task(polling_loop())
-                
-            async def power_on(robot, buttons: list):
-                for button in buttons:
-                    button.disable()
-                n = ui.notification(timeout=None)
-                for i in range(1):
-                    n.message = 'Powering on...'
-                    n.spinner = True
-                    await asyncio.sleep(1)
-                    
-                await start_container(robot)
-                
-                n.message = 'Container started'
-                n.type = 'positive'
-                n.spinner = False
-                n.timeout = 4
-                
-                displayCards.refresh()
-
-            async def power_off(robot, buttons: list):
-                for button in buttons:
-                    button.disable()
-                n = ui.notification(timeout=None)
-                for i in range(1):
-                    n.message = 'Powering off...'
-                    n.spinner = True
-                    await asyncio.sleep(1)
-                    
-                message, type = stop_container(robot)
-                
-                n.message = message
-                n.type = type
-                n.spinner = False
-                n.timeout = 4
-                
-                displayCards.refresh()
-
-            async def reboot(robot, buttons: list):
-                for button in buttons:
-                    button.disable()
-                n = ui.notification(message='Rebooting...', spinner=True, timeout=None)
-
-                await restart_container(robot)
-                
-                n.message = 'Done'
-                n.spinner = False
-                n.type = 'positive'
-                n.timeout = 4
-                
-                displayCards.refresh()
-                
-            async def add_to_world(robot):
-                try:
-                    robotType = "_".join(str(robot).split('_')[0:-1])
-                    print(robotType)
-                    await gz_world.prep_bot(robot, robotType)
-                    running_worlds = await get_running_worlds()
-                    if len(running_worlds) > 0:
-                        ui.notify(f'Added {robot} to world')
-                        return True
-                    else:
-                        raise Exception('No world running')
-                except Exception as e:
-                    print(e)
-                    return False
-
-            async def remove_from_world(robot):
-                try:
-                    robot = gz_world.models[robot].kill_model()
-                    return True
-                except Exception as e:
-                    print(e)
-                    return False
-                
-            async def delete(n):
-                notif = ui.notification(timeout=False)
-                for i in range(1):
-                    notif.message = 'Deleting...'
-                    notif.spinner=True
-                    await asyncio.sleep(0.1)
-
-                if await delete_robot(n):
-                    notif.message = f'{n} deleted'
-                    notif.type = 'positive'
-                else:
-                    notif.message = 'error deleting robot'
-                    notif.type = 'negative'
-                
-                notif.spinner = False
-                notif.timeout = 4
 
             # Card and details
             with ui.card().classes('p-[calc(var(--nicegui-default-padding)*1.2)] w-[calc(50%-(var(--nicegui-default-gap)/2))] h-auto'):
@@ -212,7 +211,7 @@ def displayCards():
                     with ui.card_section().classes('w-full p-0 mb-2'):
                         with ui.row():
                             command = f"DOCKER_HOST=unix:///tmp/dind-sockets/spirisdk_{robotName}.socket"
-                            ui.code(command, language='bash').classes('text-sm text-gray-600 dark:text-gray-200')
+                            ui.code(command, language='bash').classes('text-gray-600 dark:text-gray-200')
 
                     # IP and web interface link
                     with ui.card_section().classes('w-full p-0 mb-2'):
@@ -222,11 +221,11 @@ def displayCards():
                             # Link to the robot's web interface if applicable
                             if "spiri_mu" in robotName:
                                 url = f'http://{daemons[robotName].get_ip()}:{80}'
-                                ui.link(f'Access the Web Interface at: {url}', url, new_tab=True).classes('text-sm dark:text-gray-200 py-3')
+                                ui.link(f'Access the Web Interface at: {url}', url, new_tab=True).classes('dark:text-gray-200 py-3')
                                         
                             if 'ARC' in robotName:
                                 url = f'http://{daemons[robotName].get_ip()}:{80}'
-                                ui.link(f'Access the Web Interface at: {url}', url, new_tab=True).classes('text-sm dark:text-gray-200 py-3')
+                                ui.link(f'Access the Web Interface at: {url}', url, new_tab=True).classes('dark:text-gray-200 py-3')
                 else:
                     with ui.card_section().classes('w-full p-0 mb-2'):
                         ui.label('Robot stats not available')
