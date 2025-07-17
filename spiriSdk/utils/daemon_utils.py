1e989dd036250a29bafb6b495b63a3d9209614cb
AxisShine <alicejsedgwick2004@gmail.com>
2025-07-17 13:40:36 +0000

    type hints and adding dependance

--- spiriSdk/utils/daemon_utils.py
+++ spiriSdk/utils/daemon_utils.py
@@ -3,14 +3,14 @@ import docker, docker.errors, time
 from nicegui import run
 from loguru import logger
 
-from spiriSdk.utils import SDKRobot
+from spiriSdk.utils.SDKRobot import SDKRobot
 from spiriSdk.settings import SDK_ROOT
 
 DATA_DIR = SDK_ROOT / 'data'
 ROBOTS_DIR = SDK_ROOT / 'robots'
 ROOT_DIR = SDK_ROOT
 
-robots = {}
+robots : dict[str, SDKRobot]= {}
 active_sys_ids = []
 
 async def init_robots():
@@ -23,10 +23,8 @@ async def init_robots():
 
         if robot_dir.exists():
             logger.debug(f"Starting a daemon for: {robot_name}")
-            new_robot = SDKRobot(robot_name, folder=robot_dir)
+            new_robot = SDKRobot(robot_name, folder=ROBOTS_DIR / robot_name / 'services')
             robots[robot_name] = new_robot
-
-            await run.io_bound(new_robot.start())
             displayCards.refresh()
 
             robot_sys = str(robot_name).rsplit('_', 1)
@@ -34,8 +32,7 @@ async def init_robots():
             
     logger.debug(f"Starting services for {len(robots)} robots...")
     for robot in robots.values():
-        message = await robot.start_services()
-        logger.info(message)
+        await run.io_bound(robot.start())
         
     logger.success("Docker robots initialized.")
 
@@ -45,32 +42,11 @@ async def start_container(robot_name):
     await run.io_bound(robots[robot_name].start())
 
 
-def stop_container(robot_name):
+async def stop_container(robot_name):
     if robot_name not in robots:
         return f"No daemon found for {robot_name}.", 'negative'
 
-    container = robots[robot_name].container
-    try:
-        container.stop()
-    except Exception as e:
-        logger.error(f"Error stopping container {robot_name}: {e}")
-        return f"Error stopping container {robot_name}: {str(e)}", 'negative'
-
-    while True:
-        try:
-            container.reload()  # Refresh container status
-            status = container.status
-            if status == "exited" or status == "stopped":
-                break
-        except docker.errors.NotFound:
-            # Container has been removed, consider it stopped
-            break
-        except Exception as e:
-            logger.error(f"Error checking container status for {robot_name}: {e}")
-            return(f'Error checking container status for {robot_name}: {e}'), 'negative'
-
-        time.sleep(1)
-        logger.debug(f"Waiting for container {robot_name} to stop... {status}")
+    await robots[robot_name].stop()
         
     logger.success(f'Container {robot_name} stopped')
     return f"Container {robot_name} stopped", 'positive'
